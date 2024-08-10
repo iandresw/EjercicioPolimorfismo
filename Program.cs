773185bba6fbd088e33935c9e9fa242553fa1b70
iandresw <rbejarano@unah.hn>
2024-08-09 23:46:02 -0600

    Update Program.cs
    se mando a llamar clase alumno y clase maestro y se imprimió con el método imprimir de cada uno de ellos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EjercicioPolimorfismo;
+
+
+Console.WriteLine("########### INICIO EJERCICIO POLIMORFISMO ###########");
+
+Maestro maestro = new Maestro();
+Console.WriteLine("Ingrese el Nombre del Maestro: ");
+maestro.PNombre = Console.ReadLine();
+Console.WriteLine("Ingrese el Apellido del Maestro: ");
+maestro.PApellido = Console.ReadLine();
+Console.WriteLine("Ingrese el DNI del Maestro: ");
+maestro.identidad = Console.ReadLine();
+maestro.Imprimir();
+
+Alumno alumno = new Alumno();
+
+Console.WriteLine("Ingrese el Nombre del Alumno: ");
+alumno.PNombre = Console.ReadLine();
+Console.WriteLine("Ingrese el Apellido del Alumno: ");
+alumno.PApellido = Console.ReadLine();
+Console.WriteLine("Ingrese la identidad del empleado cajero: ");
+alumno.identidad = Console.ReadLine();
+alumno.Imprimir();
