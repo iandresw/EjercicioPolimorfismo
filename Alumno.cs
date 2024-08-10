7daf920313dff0346798b86a1056f7ce321391f0
iandresw <rbejarano@unah.hn>
2024-08-09 23:45:02 -0600

    Create Alumno.cs
    se creo la clase alumno y se heredo de la clase persona y se creo un metodo bool si esta matriculado o no

---
+++ Alumno.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPolimorfismo
+{
+    public class Alumno : Persona
+    {
+        public override string DNI()
+        {
+            return identidad;
+        }
+
+        public override string Nombre()
+        {
+            return PNombre + " " + PApellido;
+        }
+
+        public bool Matriculado()
+        {
+            return true;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.WriteLine("## DATOS EMPLEADO ALUMNO ##");
+            Console.WriteLine("\n");
+            Console.WriteLine($"Nombre empleado: {Nombre()}");
+            Console.WriteLine($"DNI: {DNI()}");
+            Console.WriteLine($"Matriculado: {(Matriculado() ? "Sí" : "No")}");
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.WriteLine("## ULTIMA LINEA ##");
+            Console.WriteLine("\n");
+        }
+    }
+}
